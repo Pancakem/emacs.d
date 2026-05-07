6cf75274faa6c51b792b27505bedeeeea667ca67
pancakem <pancakesdeath@protonmail.com>
2026-05-07 14:56:54 +0300

    update typst

--- init.el
+++ init.el
@@ -818,9 +818,7 @@
 
 
 (use-package typst-ts-mode
-  :elpaca (:type git :host sourcehut :repo "meow_king/typst-ts-mode")
-  :custom
-  (typst-ts-mode-watch-options "--open"))
+  :ensure t)
 
 ;; Static analysis
 (use-package flycheck-clang-analyzer
